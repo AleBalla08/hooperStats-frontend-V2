6e0993977fd4a00938174282569dba534c327d80
Alexandre Ballardin <aleballa08@gmail.com>
2026-03-09 09:43:24 -0300

    adicionando tela de treinos e dash

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,16 @@
 import { Routes } from '@angular/router';
+import { WorkoutsComponent } from './components/workouts/workouts.component';
 
 export const routes: Routes = [
   {
     path: 'home',
     loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+  },
+  {
+    path: 'treinos',
+    loadComponent: () =>
+      import('./components/workouts/workouts.component')
+        .then((m) => m.WorkoutsComponent),
   },
   {
     path: '',
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,16 +2,21 @@ import { Component } from '@angular/core';
 import { SideMenuComponent } from "../components/side-menu/side-menu.component";
 import { IonicModule } from '@ionic/angular';
 import { addIcons } from 'ionicons';
-import {basketball} from 'ionicons/icons';
+import {basketball, add, receipt} from 'ionicons/icons';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonicModule, SideMenuComponent],
+  imports: [
+    IonicModule,
+    SideMenuComponent,
+    RouterModule
+  ],
 })
 export class HomePage {
   constructor() {
-    addIcons({basketball});
+    addIcons({basketball, add, receipt});
   }
 }
