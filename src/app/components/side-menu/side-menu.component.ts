34ffd9be33e35db503fc8d2bc07fe7dc900ab31c
Alexandre Ballardin <aleballa08@gmail.com>
2026-03-10 09:34:21 -0300

    iniciando tela de treinos

--- src/app/components/side-menu/side-menu.component.ts
+++ src/app/components/side-menu/side-menu.component.ts
@@ -1,20 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 import { IonIcon } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import {home, person, barbell, sparkles} from 'ionicons/icons';
+import {home, person, barbell, sparkles, receipt} from 'ionicons/icons';
 
 @Component({
   selector: 'app-side-menu',
   templateUrl: './side-menu.component.html',
   styleUrls: ['./side-menu.component.scss'],
-  imports: [CommonModule, IonicModule]
+  imports: [CommonModule, IonicModule, RouterModule]
 })
 export class SideMenuComponent  implements OnInit {
 
   constructor() { 
-     addIcons({home, person, barbell, sparkles});
+     addIcons({home, person, barbell, sparkles, receipt});
   }
 
   ngOnInit() {}
--- src/app/components/workouts/workouts.component.ts
+++ src/app/components/workouts/workouts.component.ts
@@ -1,13 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { SideMenuComponent  } from '../side-menu/side-menu.component';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { addIcons } from 'ionicons';
+import { add, barbell } from 'ionicons/icons';
 
 @Component({
   selector: 'app-workouts',
   templateUrl: './workouts.component.html',
   styleUrls: ['./workouts.component.scss'],
+  imports: [SideMenuComponent, CommonModule, IonicModule]
 })
 export class WorkoutsComponent  implements OnInit {
 
-  constructor() { }
+  constructor() { 
+    addIcons({barbell, add});
+  }
 
   ngOnInit() {}
 
